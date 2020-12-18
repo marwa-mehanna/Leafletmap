787b1a58c18a5527a1817d361343675d478c3864
marwa-mehanna <marwa.mehana@gmail.com>
2020-12-18 21:47:31 +0200

    adding  ui.R & server.R

---
+++ InteractiveMap/server.R
@@ -0,0 +1,24 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(leaflet)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    output$map <- renderLeaflet({
+        df %>%
+            leaflet() %>%
+            addTiles() %>%
+            addMarkers(lat =-8.5098,lng = 115.2654)
+        
+        
+    })
+    
+})
